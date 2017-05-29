219653e3a14e8cbb3da00ab054b9d305cf3a6ea7
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-05-29 17:10:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*
-  A perfect square is an integer that is the product of some integer with itself. For example, 9 is a perfect square, since it can be written as 3 * 3.
+  A perfect square is an integer that is the product of some integer with itself.
+  For example, 9 is a perfect square, since it can be written as 3 * 3.
 
   Src: https://en.wikipedia.org/wiki/Square_number
 */
