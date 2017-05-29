aa58485f8be3e84fa80b68b488dd64c47653d78a
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-05-29 16:48:07 -0300

    Add tests

---
+++ test.js
@@ -0,0 +1,30 @@
+import test from 'ava'
+import perfectSquare from './'
+
+test(t => {
+  t.true(perfectSquare(9))
+})
+
+test(t => {
+  t.false(perfectSquare(11))
+})
+
+test(t => {
+  t.true(perfectSquare(25))
+})
+
+test(t => {
+  t.true(perfectSquare(36))
+})
+
+test(t => {
+  t.false(perfectSquare(0))
+})
+
+test(t => {
+  t.false(perfectSquare(-1))
+})
+
+test(t => {
+  t.false(perfectSquare(7.5))
+})
